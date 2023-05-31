17e684a2033056076e47674bda2fe052e477e6ca
Aditya Mishra <mishra.aditya12@gmail.com>
2023-05-31 12:05:22 -0700

    WIFI OTA manually triggered, working with LORA Device

--- WifiOTA/main.py
+++ WifiOTA/main.py
@@ -1,16 +1,14 @@
 from network import LoRa, WLAN
 import socket
 import time
-from OTA import WiFiOTA
 from time import sleep
 import pycom
 import ubinascii
+from OTA import WiFiOTA
 
 from config import WIFI_SSID, WIFI_PW, SERVER_IP
 
-# Turn on GREEN LED
-pycom.heartbeat(False)
-pycom.rgbled(0xff00)
+pycom.heartbeat(True)
 
 # Setup OTA
 ota = WiFiOTA(WIFI_SSID,
@@ -30,24 +28,34 @@ app_eui = ubinascii.unhexlify('58A0CBFFFE803F9C')
 # Need to change the app_key
 #app_key = ubinascii.unhexlify('B57F36D88691CEC5EE8659320169A61C')
 app_key = ubinascii.unhexlify('E82511CC86A1FF6F8AEC6238920225DA')
+dev_eui = ubinascii.unhexlify('70B3D5499A2B29C2')
+
+# Uncomment for US915 / AU915 & Pygate
+for i in range(0,8):
+    lora.remove_channel(i)
+for i in range(16,65):
+    lora.remove_channel(i)
+for i in range(66,72):
+    lora.remove_channel(i)
 
 # join a network using OTAA (Over the Air Activation)
-lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
 # wait until the module has joined the network
 while not lora.has_joined():
     time.sleep(2.5)
     print('Not yet joined...')
-
+print("joined!")
 # create a LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-
+print("Socket created!")
 # set the LoRaWAN data rate - need to set to 3 for USA
 s.setsockopt(socket.SOL_LORA, socket.SO_DR, 3)
-
+print("Data Rate Set!")
 # make the socket blocking
 # (waits for the data to be sent and for the 2 receive windows to expire)
-s.setblocking(True)
+s.setblocking(False)
+print("Blocking NOT Set!")
 
 while True:
     # send some data
@@ -61,6 +69,8 @@ while True:
     data = s.recv(64)
 
     # Some sort of OTA trigger
+    data = bytes([0x01, 0x02, 0x03])
+    print("Hardcoded Trigger -> should start OTA")
     if data == bytes([0x01, 0x02, 0x03]):
         print("Performing OTA")
         # Perform OTA
