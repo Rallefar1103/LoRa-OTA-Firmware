04c0c075d5cc911336e643488e72356fe2faa943
MikeLi <18688161372lz@gmail.com>
2023-06-10 15:40:31 -0700

    updating main with more print

--- OTA/1.0.0/flash/main.py
+++ OTA/1.0.0/flash/main.py
@@ -33,6 +33,15 @@ app_eui = ubinascii.unhexlify('58A0CBFFFE803F9C')
 app_key = ubinascii.unhexlify('E82511CC86A1FF6F8AEC6238920225DA')
 dev_eui = ubinascii.unhexlify('70B3D5499A2B29C2')
 
+# Uncomment for US915 / AU915 & Pygate
+for i in range(0, 8):
+    lora.remove_channel(i)
+for i in range(16, 65):
+    lora.remove_channel(i)
+for i in range(66, 72):
+    lora.remove_channel(i)
+
+
 # join a network using OTAA (Over the Air Activation)
 lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 
@@ -41,15 +50,21 @@ while not lora.has_joined():
     time.sleep(2.5)
     print('Not yet joined...')
 
+print('Joined')
+
 # create a LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
+print("Socket Created")
+
 # set the LoRaWAN data rate
 s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+print("Set Socket Data Rate")
 
 # make the socket blocking
 # (waits for the data to be sent and for the 2 receive windows to expire)
 s.setblocking(True)
+print("Set Socket Blocking")
 
 while True:
     # send some data
@@ -58,6 +73,14 @@ while True:
     # make the socket non-blocking
     # (because if there's no data received it will block forever...)
     s.setblocking(False)
+    s.settimeout(3.0)
+
+    try:
+        rx_pkt = s.recv(64)
+        print("Received data: {}".format(rx_pkt))
+    except socket.timeout:
+        print("No data received")
+
 
     # get any data received (if any...)
     data = s.recv(64)
@@ -70,3 +93,4 @@ while True:
         ota.update()
 
     sleep(5)
+
