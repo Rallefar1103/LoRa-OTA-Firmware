3adce5db08a08fb38595beb91a1a007d2c91c16b
Turan Vural <turanzv@gmail.com>
2023-06-10 15:52:19 -0700

    template for LoRaOTA class

--- OTA/1.0.0/flash/lib/OTA.py
+++ OTA/1.0.0/flash/lib/OTA.py
@@ -256,3 +256,106 @@ class WiFiOTA(OTA):
                 return bytes(content)
         elif hash:
             return hash_val
+
+# TODO switch implementation form WiFiOTA to LoRaOTA
+class LoRaOTA(OTA):
+    def __init__(self, ssid, password, ip, port):
+        self.SSID = ssid
+        self.password = password
+        self.ip = ip
+        self.port = port
+
+    def connect(self):
+        self.wlan = network.WLAN(mode=network.WLAN.STA)
+        if not self.wlan.isconnected() or self.wlan.ssid() != self.SSID:
+            for net in self.wlan.scan():
+                if net.ssid == self.SSID:
+                    self.wlan.connect(self.SSID, auth=(network.WLAN.WPA2,
+                                                       self.password))
+                    while not self.wlan.isconnected():
+                        machine.idle()  # save power while waiting
+                    break
+            else:
+                raise Exception("Cannot find network '{}'".format(SSID))
+        else:
+            # Already connected to the correct WiFi
+            pass
+
+    def _http_get(self, path, host):
+        req_fmt = 'GET /{} HTTP/1.0\r\nHost: {}\r\n\r\n'
+        req = bytes(req_fmt.format(path, host), 'utf8')
+        return req
+
+    def get_data(self, req, dest_path=None, hash=False, firmware=False):
+        h = None
+
+        # Connect to server
+        print("Requesting: {}".format(req))
+        s = socket.socket(socket.AF_INET,
+                          socket.SOCK_STREAM,
+                          socket.IPPROTO_TCP)
+        s.connect((self.ip, self.port))
+
+        # Request File
+        s.sendall(self._http_get(req, "{}:{}".format(self.ip, self.port)))
+
+        try:
+            content = bytearray()
+            fp = None
+            if dest_path is not None:
+                if firmware:
+                    raise Exception("Cannot write firmware to a file")
+                fp = open(dest_path, 'wb')
+
+            if firmware:
+                pycom.ota_start()
+
+            h = uhashlib.sha1()
+
+            # Get data from server
+            result = s.recv(100)
+
+            start_writing = False
+            while (len(result) > 0):
+                # Ignore the HTTP headers
+                if not start_writing:
+                    if "\r\n\r\n" in result:
+                        start_writing = True
+                        result = result.decode().split("\r\n\r\n")[1].encode()
+
+                if start_writing:
+                    if firmware:
+                        pycom.ota_write(result)
+                    elif fp is None:
+                        content.extend(result)
+                    else:
+                        fp.write(result)
+
+                    if hash:
+                        h.update(result)
+
+                result = s.recv(100)
+
+            s.close()
+
+            if fp is not None:
+                fp.close()
+            if firmware:
+                pycom.ota_finish()
+
+        except Exception as e:
+            # Since only one hash operation is allowed at Once
+            # ensure we close it if there is an error
+            if h is not None:
+                h.digest()
+            raise e
+
+        hash_val = ubinascii.hexlify(h.digest()).decode()
+
+        if dest_path is None:
+            if hash:
+                return (bytes(content), hash_val)
+            else:
+                return bytes(content)
+        elif hash:
+            return hash_val
