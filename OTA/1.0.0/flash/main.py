e4900fbd6f1cb4824c901b296f8989e07b1f25d4
Aditya Mishra <mishra.aditya12@gmail.com>
2023-06-14 00:07:20 -0700

    Fixed retransmission error, no more duplicates coming from buffer - we now use a list with fixed size to ensure no duplicates

--- OTA/1.0.0/flash/main.py
+++ OTA/1.0.0/flash/main.py
@@ -70,21 +70,21 @@ s.setblocking(True)
 print("Set Socket Blocking")
 
 # creat a object to hold firmware files
-data = []
+# data = []
 
 flag = True
 # s.send(bytes([0x01, 0x02, 0x03]))
-s.settimeout(15.0) # configure a timeout value of 3 seconds
+s.settimeout(10.0) # configure a timeout value of 3 seconds
 try:
     s.send(bytes([0x03, 0x02, 0x01]))
     print("Sending First 3, 2, 1")
     num_seg = s.recv(64)
     num_seg = int.from_bytes(num_seg, "big")
     print("Received num_seg is {}".format(num_seg))
-    s.settimeout(5.0)
+    s.settimeout(10.0)
 except socket.timeout:
     print("Socket Timed Out, manual retranmission to get num_seg")
-    s.send(bytes([0x03, 0x02, 0x01]))
+    s.send(bytes([0x01, 0x02, 0x03]))
     print("Sending First 3, 2, 1")
     num_seg = s.recv(64)
     num_seg = int.from_bytes(num_seg, "big")
@@ -96,10 +96,13 @@ except socket.timeout:
 # print("Received rx_pkt is {}".format(rx_pkt))
 
 start_time = time.time()
-data = []
+data = [None] * num_seg
 
 receiving_failed=False
-for i in range (num_seg - 1):
+i = 0
+
+while i < (num_seg): # check bounds, I think this should be num_seg
+    # i+=1
     try:
         if(receiving_failed):
             s.send(bytes([0x01, 0x02, 0x03]))
@@ -108,12 +111,18 @@ for i in range (num_seg - 1):
         print("Sending {} iter: 3, 2, 1".format(i))
         rx_pkt = s.recv(64)
         receiving_failed = False
-        data.append(rx_pkt)
-        print("Received rx_pkt is {}".format(rx_pkt))
+        # data.append(rx_pkt)
+        # print("data was techncally received...")
+        # print("The value of i is {} and data[i] is {}".format(i, data[i]))
+        data[i]=rx_pkt
+        # print("UPDATED MEMORY")
+        print("The value of i is {} and data[i] is {}".format(i, data[i]))
+        print("Received rx_pkt for iter {} is {}".format(i, rx_pkt))
+        i+=1
     except socket.timeout:
         print("Socket Timed Out, retransmitting for iteration {}".format(i))
-        i-=1
-        print("New value of i is {}".format(i))
+        # i-=1 # Need to check if this is a proper way to edit iterator in for loop, it may not be...
+        # print("New value of i is {}".format(i))
         receiving_failed = True
         # print('No packet received')
         # s.send(bytes([0x01, 0x02, 0x03]))
@@ -131,116 +140,13 @@ for i in range (num_seg - 1):
 end_time_1 = time.time()
 print("Time to move file over LoRa was: {} seconds".format(end_time_1-start_time))
 # Write 2d binary array data to a file
-with open('/flash/ota.bin', 'wb') as f:
+
+# ota.bin 
+with open('/flash/ota_2.bin', 'wb') as f:
     for i in range(len(data)):
         f.write(data[i])
 end_time_2 = time.time()
 
 print("Time to Flash Firmware was {} seconds".format(end_time_2-end_time_1))
 print("Time for Entire Process was  {} seconds".format(end_time_2-start_time))
-
-
-# while flag:
-#     # print("Getting first 1, 2, 3")
-#     # # Create a loop to take the segments of the file and store them in the data object
-#     # # rx_pkt = s.recv(64)
-#     # # print("Received data: {}".format(rx_pkt))
-#     # # Check if the data received is the start of the OTA\
-#     # print("Check to see if received Data is 1 , 2, 3")
-#     # if rx_pkt == bytes([0x01, 0x02, 0x03]):
-#     #     print("Performing OTA")
-#     #     # Get the number of segments
-#     #     s.send(bytes([0x03, 0x02, 0x01]))
-#     num_seg = s.recv(64)
-#     print("Received num_seg is {}".format(num_seg))
-
-#     while flag:
-#         num_seg = s.recv(64)
-#         print("Total Length: {}".format(num_seg))
-#         # Wait for the number of segments to be received
-#         if num_seg != b'':
-#             # Convert the byte received to an integer
-#             num_seg = int.from_bytes(num_seg, "big")
-#             print("Total Length: {}".format(num_seg))
-#             s.send(bytes([0x03, 0x02, 0x01]))
-
-#             # using the number of segments, loop through and get the data
-#             while len(data) < num_seg:
-#                 data_pkt = s.recv(64)
-#                 # only append the data if the data_pkt is not empty
-#                 if data_pkt != b'':
-#                     print("Received data: {}".format(data_pkt))
-#                     data.append(data_pkt)
-#                     print("Data len is : {}", len(data))
-#                     s.send(bytes([0x03, 0x02, 0x01]))
-#                 # sleep(1)
-#             flag = False
-#             # sleep(1)
-#     # sleep(1)
-
-# # Write 2d binary array data to a file
-# with open('/flash/ota.bin', 'wb') as f:
-#     for i in range(len(data)):
-#         f.write(data[i])
-
-# data = []
-# while True:
-#     # get any data received (if any...)
-#     rx_pkt = s.recv(64)
-#     print("Received data: {}".format(rx_pkt))
-#     if rx_pkt == bytes([0x01, 0x02, 0x03]):
-#         print("Performing OTA")
-#         # Perform OTA
-#         ota.connect()
-#         ota.update()
-#     else:
-#         data.append(rx_pkt)
-#         print("Data: {}".format(data))
-
-#     sleep(5)
-
-
-
-
-# # send some data
-# s.send(bytes([0x01, 0x02, 0x03]))
-# while True:
-
-
-#     # make the socket non-blocking
-#     # (because if there's no data received it will block forever...)
-#     s.setblocking(True)
-#     # ms.settimeout(60.0)
-
-#     try:
-#         rx_pkt = s.recv(64)
-#         print("Received data: {}".format(rx_pkt))
-#     except socket.timeout:
-#         print("No data received")
-
-
-#     # get any data received (if any...)
-#     # data = s.recv(64)
-
-#     # Some sort of OTA trigger
-#     if rx_pkt == bytes([0x01, 0x02, 0x03]):
-#         print("Performing OTA")
-#         s.send(bytes([0x03, 0x02, 0x01]))
-#         print("Socket send 3,2,1")
-#         sleep(1)
-
-#         # Check if received packet is none-empty
-#         rx_pkt = s.recv(64)
-#         while (rx_pkt == b''):
-#             rx_pkt = s.recv(64)
-#             print("Receiving Empty packet")
-#             sleep(1)
-
-#         num_seg = int.from_bytes(rx_pkt, "big")
-#         print("Total Length: {}".format(num_seg))
-        
-#         # Perform OTA
-#         # ota.connect()
-#         # ota.update()
-#     sleep(5)
 
